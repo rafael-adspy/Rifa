3637e5dd8553402cdf2ad2fa21bf804d61d12adb
rafael-adspy <rafael145007@icloud.com>
2026-02-09 20:57:20 -0300

    Add files via upload

---
+++ ex00sorteio.py
@@ -0,0 +1,8 @@
+import random
+print(' Sorteio da rifa: ')
+print(' 1 - Alan')
+print(' 2 - Joao')
+print(' 3 - Jose ')
+print(' 4 - Noel ')
+g = random.randint(1,4)
+print('O ganhador da rifa é: {}'.format(g))
